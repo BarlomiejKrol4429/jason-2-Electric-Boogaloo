e9c7eea34075161fb788dabebfcc229c99708a4c
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2025-12-05 12:11:18 +0100

    Łączenie z internetem i odczyt pytań

--- app/src/main/java/com/example/jason/MainActivity.java
+++ app/src/main/java/com/example/jason/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.jason;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +30,64 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://my-json-server.typicode.com/BarlomiejKrol4429/json/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JavaPlaceHolderApi javaPlaceHolderApi
+                = retrofit.create(JavaPlaceHolderApi.class);
+        Call<List<Pytanie>> call = javaPlaceHolderApi.getPytania();
+        call.enqueue(
+                new Callback<List<Pytanie>>() {
+                    @Override
+                    public void onResponse(Call<List<Pytanie>> call, Response<List<Pytanie>> response) {
+                        if(!response.isSuccessful()){
+                            Toast.makeText(MainActivity.this,
+                                    ""+response.code(),
+                                    Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        List<Pytanie> pytania = response.body();
+                        Toast.makeText(MainActivity.this, pytania.get(0).getTrescPytania(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Pytanie>> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
+/*
+Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://my-json-server.typicode.com/mechaniktgmobilne/retrofit_pytania/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Pytanie>> call =jsonPlaceHolderApi.getPytania();
+        call.enqueue(
+                new Callback<List<Pytanie>>() {
+                    @Override
+                    public void onResponse(Call<List<Pytanie>> call, Response<List<Pytanie>> response) {
+                        if(!response.isSuccessful()){
+                            Toast.makeText(MainActivity.this,
+                                    ""+response.code(),
+                                    Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        pytania = response.body();
+                        Toast.makeText(MainActivity.this,
+                                pytania.get(0).getTrescPytania()
+                                , Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Pytanie>> call, Throwable t) {
+                        Toast.makeText(MainActivity.this,
+                                ""+t.getMessage(), Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                }
+        );
+ */
