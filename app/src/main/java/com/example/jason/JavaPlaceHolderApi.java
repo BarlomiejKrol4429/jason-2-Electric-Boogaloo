d7a108859523044960fdd6bed10d9de64b53b022
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2025-12-05 11:57:00 +0100

    Inerface api

---
+++ app/src/main/java/com/example/jason/JavaPlaceHolderApi.java
@@ -0,0 +1,11 @@
+package com.example.jason;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JavaPlaceHolderApi {
+    @GET("pytania")
+    public Call<List<Pytanie>> getPytania();
+}
